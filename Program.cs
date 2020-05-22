98c8b5ffec814e112ce5bb5f5e7a61a9b3fc5c68
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-22 20:31:33 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Soma2numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string numero1;
+            string numero2;
+
+            double numero01;
+            double numero02;
+
+            double valor;
+
+            Console.WriteLine("Digite um primeiro número....");
+            numero1 = Console.ReadLine();
+            Console.WriteLine("Digite um segundo...");
+            numero2 = Console.ReadLine();
+
+            numero01 = Convert.ToDouble(numero1);
+            numero02 = Convert.ToDouble(numero2);
+            
+            valor = numero01 + numero02;
+
+            Console.WriteLine(valor);
+
+
+        }
+    }
+}
